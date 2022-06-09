00668d4fa8ccdbbbf4aabf3b4de47f60626dcad7
bohdanbershadskyi <69502515+bohdanbershadskyi@users.noreply.github.com>
2022-06-09 11:22:05 +0300

    add more test to auth middleware

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,15 +1,62 @@
 import { expect } from "chai";
+import jwt from "jsonwebtoken";
+import sinon from "sinon";
 
 import authMiddleware from "../middleware/is-auth.js";
 
-it("shoud throw an error if no authorization header is present", () => {
-  const req = {
-    get: (headerName) => {
-      return null;
-    },
-  };
+describe("Auth middleware", () => {
+  it("shoud throw an error if no authorization header is present", () => {
+    const req = {
+      get: (headerName) => {
+        return null;
+      },
+    };
 
-  expect(() => {
+    expect(() => {
+      authMiddleware(req, {}, () => {});
+    }).to.throw("Token verifying failed");
+  });
+
+  it("shoud throw an error if the authorization header is only one string", () => {
+    const req = {
+      get: (headerName) => {
+        return "xxx";
+      },
+    };
+
+    expect(() => {
+      authMiddleware(req, {}, () => {});
+    }).to.throw("Token verifying failed");
+  });
+
+  it("shoud throw an error if the token cannot be verified", () => {
+    const req = {
+      get: (headerName) => {
+        return "Bearer xxx";
+      },
+    };
+
+    expect(() => {
+      authMiddleware(req, {}, () => {});
+    }).to.throw("Token verifying failed");
+  });
+
+  it("shoud yield a userId after decoding the token", () => {
+    const req = {
+      get: (headerName) => {
+        return "Bearer xxx";
+      },
+    };
+
+    sinon.stub(jwt, "verify");
+    jwt.verify.returns({ userId: "abc" });
+    // jwt.verify = () => {
+    //   return { userId: "abc" };
+    // };
     authMiddleware(req, {}, () => {});
-  }).to.throw("Token verifying failed");
+    expect(req).to.have.property("userId");
+    expect(req).to.have.property("userId", "abc");
+    expect(jwt.verify.called).to.be.true;
+    jwt.verify.restore();
+  });
 });
